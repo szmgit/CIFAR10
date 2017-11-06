debca98b73546fff8b87ca011c4e07b1fc7f8f02
SUN_ZM <cry_ptonym@126.com>
2017-11-06 22:32:06 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -10,10 +11,10 @@ transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root='/home/shixiao/szm/data', train=True, download=False, transform=transform)
+trainset = torchvision.datasets.CIFAR10(root='/home/szm/data', train=True, download=False, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='/home/shixiao/szm/data', train=False, download=False, transform=transform)
+testset = torchvision.datasets.CIFAR10(root='/home/szm/data', train=False, download=False, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
